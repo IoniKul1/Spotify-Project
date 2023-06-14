c5ec47cc16e5668ea3e1f45e757c4689ce8c59d7
tinog87 <martingaido87@gmail.com>
2023-06-14 09:25:52 -0300

    j

--- repro music/music.js
+++ repro music/music.js
@@ -1,62 +1,59 @@
 window.onload = function() {
-    var songGraph = document.getElementById("song-graph");
-    var ctx = songGraph.getContext("2d");
-  
-    var songSelect = document.getElementById("song-select");
-    var audioPlayer = document.getElementById("audio-player");
-    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    var audioSource = audioContext.createMediaElementSource(audioPlayer);
-    var analyser = audioContext.createAnalyser();
-  
-    // Configuración del analizador de audio
-    analyser.fftSize = 2048;
-    var bufferLength = analyser.frequencyBinCount;
-    var dataArray = new Uint8Array(bufferLength);
-  
-    audioSource.connect(analyser);
-    audioSource.connect(audioContext.destination);
-  
-    // Configuración del gráfico
-    var graphHeight = songGraph.height;
-    var graphWidth = songGraph.width;
-    var barWidth = graphWidth / bufferLength;
-    var maxBarHeight = graphHeight / 2;
-  
-    // Dibuja el gráfico interactivo
-    function drawGraph() {
-      analyser.getByteFrequencyData(dataArray);
-  
-      ctx.clearRect(0, 0, graphWidth, graphHeight);
-  
-      ctx.fillStyle = "#239419";
-  
-      var x = 0;
-  
-      for (var i = 0; i < bufferLength; i++) {
-        var barHeight = dataArray[i] / 255 * maxBarHeight;
-  
-        ctx.fillRect(x, graphHeight - barHeight, barWidth, barHeight);
-  
-        x += barWidth;
+  // Obtener elementos del DOM
+  var canvas = document.getElementById('visualizer');
+  var audio = document.getElementById('audio');
+  var context = new AudioContext();
+  var src = context.createMediaElementSource(audio);
+  var analyser = context.createAnalyser();
+
+  // Conectar nodos de audio
+  src.connect(analyser);
+  analyser.connect(context.destination);
+
+  // Configurar el lienzo para visualización
+  var canvasCtx = canvas.getContext('2d');
+  var bufferLength = analyser.frequencyBinCount;
+  var dataArray = new Uint8Array(bufferLength);
+
+  function draw() {
+    // Realizar la animación de visualización
+    requestAnimationFrame(draw);
+
+    // Obtener datos de amplitud del analizador
+    analyser.getByteTimeDomainData(dataArray);
+
+    // Limpiar el lienzo con transparencia
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Configurar los estilos de visualización
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 255, 0)';
+    canvasCtx.beginPath();
+
+    var sliceWidth = canvas.width * 1.0 / bufferLength;
+    var x = 0;
+
+    // Dibujar la forma de onda en el lienzo
+    for (var i = 0; i < bufferLength; i++) {
+      var v = dataArray[i] / 128.0;
+      var y = v * canvas.height / 2;
+
+      if (i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
       }
-  
-      requestAnimationFrame(drawGraph);
+
+      x += sliceWidth;
     }
-  
-    // Cambiar la canción cuando se seleccione una opción diferente
-    songSelect.addEventListener("change", function() {
-      var selectedSong = songSelect.value;
-      audioPlayer.src = selectedSong;
-      audioContext.resume().then(() => {
-        drawGraph();
-      });
-    });
-  
-    // Reproducir la canción y dibujar el gráfico
-    audioPlayer.addEventListener("play", function() {
-      audioContext.resume().then(() => {
-        drawGraph();
-      });
-    });
+
+    canvasCtx.lineTo(canvas.width, canvas.height / 2);
+    canvasCtx.stroke();
+  }
+
+  // Iniciar la visualización cuando se reproduce el audio
+  audio.onplay = function() {
+    context.resume();
+    draw();
   };
-  
+};
